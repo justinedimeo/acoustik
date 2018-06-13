fa69f65cbe5097171b37e46c484e0b6c7e4c3fe1
justinedimeo <dimeo.justine@gmail.com>
2018-06-13 16:49:30 +0200

    🤘 Site > Home page

--- dist/index.php
+++ dist/index.php
@@ -1,18 +1,68 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,600,700" rel="stylesheet">
-    <link href="styles/reset.css" rel="stylesheet">
-    <link href="styles/main.css" rel="stylesheet">
-    <title>Coucou</title>
-</head>
+<?php
+    include 'views/config.php';
+    
+    $query = $pdo->query('SELECT * FROM new_sounds  WHERE genre = "rock" ');
+    $rock = $query->fetchAll();
 
-<body>
-    <?php include 'views/config.php' ?>
-    <a href="views/slider.php">Page slider</a>
-    <a href="views/player.php">Page player</a>
-</body>
-</html>
+    $query = $pdo->query('SELECT * FROM new_sounds  WHERE genre = "pop"');
+    $pop = $query->fetchAll();
+
+    $query = $pdo->query('SELECT * FROM new_sounds  WHERE genre = "jazz"');
+    $jazz = $query->fetchAll();
+
+    $query = $pdo->query('SELECT * FROM new_sounds  WHERE genre = "rap"');
+    $rap = $query->fetchAll();
+
+    $track_rock = $rock[mt_rand(0, 1)];
+    $track_pop = $pop[mt_rand(0, 1)];
+    $track_jazz = $jazz[mt_rand(0, 1)];
+    $track_rap = $rap[mt_rand(0, 1)];
+?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,600,700" rel="stylesheet">
+        <link href="styles/reset.css" rel="stylesheet">
+        <link href="styles/main.css" rel="stylesheet">
+        <title>Coucou</title>
+    </head>
+
+    <body>
+        <div class="index">
+            <header style="padding-top:5vh;">
+                <h1>
+                    <a href="#">Acoustik</a>
+                </h1>
+                <!-- Search a song -->
+                <form action="views/player.php" method="get">
+                    <input type="search" name="id" placeholder="Search a music">
+                    <button type="submit">Search</button>
+                </form>
+            </header>
+            <h2>Listen to music created all around the world. Or create one and become a rock star 🤘 !</h2>
+            <div class="music-types">
+                <a class="rock" href="views/player.php?id=<?= $track_rock->id ?>">
+                    <p>Rock</p>
+                </a>
+                <a class="pop" href="views/player.php?id=<?= $track_pop->id ?>">
+                    <p>Pop</p>
+                </a>
+                <a class="rap" href="views/player.php?id=<?= $track_rap->id ?>">
+                    <p>Rap</p>
+                </a>
+                <a class="jazz" href="views/player.php?id=<?= $track_jazz->id ?>">
+                    <p>Jazz</p>
+                </a>
+            </div>
+            <div class="button">
+                <a class="create-button" href="views/slider.php">Create Music</a>
+            </div>
+        </div>
+    </body>
+
+    </html>
--- dist/views/player.php
+++ dist/views/player.php
@@ -16,91 +16,99 @@
     $sound_order = $new_sounds[$id]->sound_order;
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,600,700" rel="stylesheet">
-    <link href="../styles/reset.css" rel="stylesheet">
-    <link href="../styles/main.css" rel="stylesheet">
-    <title>Player</title>
-</head>
-<body>
+    <!DOCTYPE html>
+    <html>
 
-    <div class="player">
-        <?php include 'header.php' ?>
-        <div class="disc">
-            <div class="center-disk"></div>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,600,700" rel="stylesheet">
+        <link href="../styles/reset.css" rel="stylesheet">
+        <link href="../styles/main.css" rel="stylesheet">
+        <title>Player</title>
+    </head>
+
+    <body>
+
+        <div class="player">
+            <?php include 'header.php' ?>
+            <div class="disc">
+                <div class="center-disk"></div>
+            </div>
+            <p class="music-id">
+                <?= $id + 1 ?>
+            </p>
+            <p class="music-genre">
+                <?= $genre_ ?>
+            </p>
         </div>
-        <p class="music-id"><?= $id + 1 ?></p>
-        <p class="music-genre"><?= $genre_ ?></p>
-    </div>
 
-    <!-- Sounds -->
-    <?php foreach ($sounds as $sound): ?>
+        <!-- Sounds -->
+        <?php foreach ($sounds as $sound): ?>
         <audio class="<?= $sound->id; ?>" src="<?= $sound->rock; ?>"></audio>
-    <?php endforeach; ?>
-    <script>
-        const $sounds = Array.from(document.querySelectorAll('audio'));
+        <?php endforeach; ?>
+        <script>
+            const $sounds = Array.from(document.querySelectorAll('audio'));
 
-        // Creating playSound function
-        const playSound = (soundName) => {
-            const $sound = $sounds.find((element) => element.classList.contains(soundName));
-            $sound.currentTime = 0;
-            $sound.play();
-        };
-        
-        // Getting the sounds composing the new_sound
-        let new_sound = <?= json_encode($sound_); ?>;
-        let new_sound_order = <?= json_encode($sound_order); ?>;
-        console.log(new_sound);
-        console.log(new_sound_order);
-
-        // Pushing all the sounds id on an array without the ,
-        array_sound = [];
-        var2 = "";
-        for (let i = 0; i <= new_sound.length; i++) {
-            if (i !== (new_sound.length) && new_sound[i] !== ",") {
-                var2 = var2 + new_sound[i];                
-            } else {
-                array_sound.push(var2);
-                var2 = "";
+            // Creating playSound function
+            const playSound = (soundName) => {
+                const $sound = $sounds.find((element) => element.classList.contains(soundName));
+                $sound.currentTime = 0;
+                $sound.play();
             };
-        }; 
 
-        // Pushing all the sounds_order id on an array without the ,
-        array_sound_order = [];
-        var1 = "";
-        for (let l = 0; l <= new_sound_order.length; l++) {
-            if (l == (new_sound_order.length)) {
-                array_sound_order.push(var1);
-            } else if (new_sound_order[l] == ",") {
-                array_sound_order.push(var1);
-                var1 = "";
-            } else {
-                var1 = var1 + new_sound_order[l];
-            };
-        };
-// 
-        // Playing sounds with playSound function
-        let k = 20;;
-        let counter2 = setInterval(updateTime, 100);
-        function updateTime() {
-            if (k > 0) {
-                for (let j = 0; j <= array_sound.length; j++) {
-                    if (k == array_sound_order[j]) {
-                        playSound(array_sound[j]);
-                        console.log(array_sound[j]);
-                        console.log(array_sound_order[j]);
-                    };
+            // Getting the sounds composing the new_sound
+            let new_sound = <?= json_encode($sound_); ?>;
+            let new_sound_order = <?= json_encode($sound_order); ?>;
+            console.log(new_sound);
+            console.log(new_sound_order);
+
+            // Pushing all the sounds id on an array without the ,
+            array_sound = [];
+            var2 = "";
+            for (let i = 0; i <= new_sound.length; i++) {
+                if (i !== (new_sound.length) && new_sound[i] !== ",") {
+                    var2 = var2 + new_sound[i];
+                } else {
+                    array_sound.push(var2);
+                    var2 = "";
                 };
-                k = (k * 10 - 0.1 * 10) / 10;
-                result = k.toFixed(1);
             };
-        };
-    </script>
-    <!-- <script src="../scripts/player.js"></script> -->
-</body>
-</html>
+
+            // Pushing all the sounds_order id on an array without the ,
+            array_sound_order = [];
+            var1 = "";
+            for (let l = 0; l <= new_sound_order.length; l++) {
+                if (l == (new_sound_order.length)) {
+                    array_sound_order.push(var1);
+                } else if (new_sound_order[l] == ",") {
+                    array_sound_order.push(var1);
+                    var1 = "";
+                } else {
+                    var1 = var1 + new_sound_order[l];
+                };
+            };
+            // 
+            // Playing sounds with playSound function
+            let k = 20;;
+            let counter2 = setInterval(updateTime, 100);
+
+            function updateTime() {
+                if (k > 0) {
+                    for (let j = 0; j <= array_sound.length; j++) {
+                        if (k == array_sound_order[j]) {
+                            playSound(array_sound[j]);
+                            console.log(array_sound[j]);
+                            console.log(array_sound_order[j]);
+                        };
+                    };
+                    k = (k * 10 - 0.1 * 10) / 10;
+                    result = k.toFixed(1);
+                };
+            };
+        </script>
+        <!-- <script src="../scripts/player.js"></script> -->
+    </body>
+
+    </html>
